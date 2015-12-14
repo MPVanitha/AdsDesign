65db3e3b295f2a2eb9558395b06275cbdc97d07a
MPVanitha <mpvanitha10@gmail.com>
2015-12-14 12:14:55 +0800

    Update myWork.js

--- js/myWork.js
+++ js/myWork.js
@@ -435,7 +435,6 @@ $(document).on('mouseout', '.appElement', function(){
 
 
 function addText() {
-	alert("coming");
 	$("#container").prepend("<div class='appElement'><div class='innerBox'><span class='newText'>"+addLineBreaks($('#adText').val())+"</span></div></div>");	
 };
 
